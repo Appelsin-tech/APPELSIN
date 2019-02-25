a8eeaef288b47f2613c9c1d869d71cce650535b8
mrPelkin <mrpelkin@gmail.com>
2019-02-25 17:42:15 +0300

    icon social

--- src/components/social-block.vue
+++ src/components/social-block.vue
@@ -3,21 +3,25 @@
     <li class="item">
       <a class="item-link" href="#">
         <img svg-inline src="@/assets/img/icon/social/vk.svg" alt="">
+        <img svg-inline src="@/assets/img/icon/social/vk-1.svg" alt="">
       </a>
     </li>
     <li class="item">
       <a class="item-link" href="#">
-        <img svg-inline src="@/assets/img/icon/social/twitter.svg" alt="">
+        <img svg-inline src="@/assets/img/icon/social/tw.svg" alt="">
+        <img svg-inline src="@/assets/img/icon/social/tw-1.svg" alt="">
       </a>
     </li>
     <li class="item">
       <a class="item-link" href="#">
-        <img svg-inline src="@/assets/img/icon/social/facebook-logo.svg" alt="">
+        <img svg-inline src="@/assets/img/icon/social/fb.svg" alt="">
+        <img svg-inline src="@/assets/img/icon/social/fb-1.svg" alt="">
       </a>
     </li>
     <li class="item">
       <a class="item-link" href="#">
-        <img svg-inline src="@/assets/img/icon/social/youtube.svg" alt="">
+        <img svg-inline src="@/assets/img/icon/social/you.svg" alt="">
+        <img svg-inline src="@/assets/img/icon/social/you-1.svg" alt="">
       </a>
     </li>
   </ul>
@@ -42,17 +46,28 @@
     .item {
       &-link {
         display: flex;
+        position: relative;
         width: 72px;
         height: 72px;
         transition: 0.3s;
         &:hover {
-          path {
-            fill: #000;
+          svg {
+            &.hover-none {
+              opacity: 0;
+            }
+            &.hover {
+              opacity: 1;
+            }
           }
         }
         >svg {
           width: 100%;
           height: 100%;
+          transition: 0.3s;
+          &.hover {
+            position: absolute;
+            opacity: 0;
+          }
           path {
             fill: #fff;
             transition: 0.3s;
