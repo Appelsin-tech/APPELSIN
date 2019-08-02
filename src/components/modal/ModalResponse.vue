0736fa6c5533d1d753a084d9f3b2e2e968167823
mrPelkin <mrpelkin@gmail.com>
2019-08-02 16:46:56 +0300

    google docs response

--- src/components/modal/ModalResponse.vue
+++ src/components/modal/ModalResponse.vue
@@ -10,94 +10,95 @@
         <div class="title-wrapper">
           <h2 class="title">Бриф на разработку</h2>
         </div>
-        <form class="form-response">
-          <div class="status">
-            <div class="status__success" v-if="response === 'success'">
-              <span>Спасибо, ваша заявка принята!</span>
-            </div>
-            <div class="status__error" v-if="response === 'error'">
-              <span>Ошибка сервера</span>
-            </div>
-          </div>
-          <div class="wrapper-description">
-            <strong class="wrapper-description__description">Подробно заполненный бриф поможет нам разработать для вас продукт в более короткие сроки. Спасибо!</strong>
-          </div>
-          <fieldset class="form-response__group group-item">
-            <h3 class="group-item__title">1. Контактная информация</h3>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="email" placeholder="Компания" v-model="form.company">
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="text" placeholder="Контактное лицо, должность" v-model="form.function_person">
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="tel" placeholder="Телефон" v-model="form.phone">
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="text" placeholder="Сайт" v-model="form.site">
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="email" placeholder="E-mail" v-model="form.email">
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="text" placeholder="Другое (например Skype)" v-model="form.other">
-            </div>
-          </fieldset>
-          <fieldset class="form-response__group group-item">
-            <h3 class="group-item__title">2. Реализуемый проект</h3>
-            <div class="g-item-form g-item-form--modals g-item-form--textarea">
-              <textarea rows="1" class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.a"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.b"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.c"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.d"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.e"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="text" placeholder="Уникальное торговое предложение" v-model="form.other">
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.f"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.g"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="text" placeholder="Список конкурентов (Список сайтов)" v-model="form.other">
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.h"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.i"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.j"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.k"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.l"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <textarea class="g-item-form__field g-item-form__field--modals g-item-form__field--textarea"  :placeholder="placeholder.two.m"></textarea>
-            </div>
-            <div class="g-item-form g-item-form--modals">
-              <input class="g-item-form__field g-item-form__field--modals" type="text" placeholder="Другое (например Skype)" v-model="form.other">
-            </div>
-          </fieldset>
-          <div class="btn-wrapper">
-            <button class="btn-wrapper__btn" type="submit">Отправить</button>
-          </div>
-        </form>
+        <!--<form class="form-response">-->
+          <!--<div class="status">-->
+            <!--<div class="status__success" v-if="response === 'success'">-->
+              <!--<span>Спасибо, ваша заявка принята!</span>-->
+            <!--</div>-->
+            <!--<div class="status__error" v-if="response === 'error'">-->
+              <!--<span>Ошибка сервера</span>-->
+            <!--</div>-->
+          <!--</div>-->
+          <!--<div class="wrapper-description">-->
+            <!--<strong class="wrapper-description__description">Подробно заполненный бриф поможет нам разработать для вас продукт в более короткие сроки. Спасибо!</strong>-->
+          <!--</div>-->
+          <!--<fieldset class="form-response__group group-item">-->
+            <!--<h3 class="group-item__title">1. Контактная информация</h3>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="email" placeholder="Компания" v-model="form.company">-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="text" placeholder="Контактное лицо, должность" v-model="form.function_person">-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="tel" placeholder="Телефон" v-model="form.phone">-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="text" placeholder="Сайт" v-model="form.site">-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="email" placeholder="E-mail" v-model="form.email">-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="text" placeholder="Другое (например Skype)" v-model="form.other">-->
+            <!--</div>-->
+          <!--</fieldset>-->
+          <!--<fieldset class="form-response__group group-item">-->
+            <!--<h3 class="group-item__title">2. Реализуемый проект</h3>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals g-item-form&#45;&#45;textarea">-->
+              <!--<textarea rows="1" class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.a"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.b"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.c"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.d"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.e"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="text" placeholder="Уникальное торговое предложение" v-model="form.other">-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.f"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.g"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="text" placeholder="Список конкурентов (Список сайтов)" v-model="form.other">-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.h"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.i"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.j"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.k"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.l"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<textarea class="g-item-form__field g-item-form__field&#45;&#45;modals g-item-form__field&#45;&#45;textarea"  :placeholder="placeholder.two.m"></textarea>-->
+            <!--</div>-->
+            <!--<div class="g-item-form g-item-form&#45;&#45;modals">-->
+              <!--<input class="g-item-form__field g-item-form__field&#45;&#45;modals" type="text" placeholder="Другое (например Skype)" v-model="form.other">-->
+            <!--</div>-->
+          <!--</fieldset>-->
+          <!--<div class="btn-wrapper">-->
+            <!--<button class="btn-wrapper__btn" type="submit">Отправить</button>-->
+          <!--</div>-->
+        <!--</form>-->
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeOoWLnELHWNXWoMNWhqwN8x6898k4Qckh9kd4q6dye7QVelg/viewform?embedded=true" width="640" height="580" frameborder="0" marginheight="0" marginwidth="0">Загрузка…</iframe>
       </div>
 
     </div>
